c194235d8913d507a49e3dd1ac6e574ad4dc0629
Alkama22 <alkalineumarliman@gmail.com>
2026-09-14 06:25:14 -0700

    creating relationship sush as one to many relationships

--- store/models.py
+++ store/models.py
@@ -2,12 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Promotion(models.Model):
+  descriptions = models.CharField(max_length=255)
+  discount = models.FloatField()
+
+class Collection(models.Model):
+  title = models.CharField(max_length=255)
+  featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name='+')
+
 class Product(models.Model):
   title = models.CharField(max_length=255)
   description  = models.TextField()
   price = models.DecimalField(max_digits=6,decimal_places=2)
   inventory = models.IntegerField()
   last_update = models.DateTimeField(auto_now=True)
+  collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
+  promotion = models.ManyToManyField(Promotion,on_delete=models.SET_NULL,null=True,related_name='+')
 
 class Customer(models.Model):
   MEMBERSHIP_BRONZE = 'B'
@@ -40,3 +50,17 @@ class Order(models.Model):
   
   placed_at = models.DateTimeField(auto_now_add=True)
   payment_status = models.CharField(max_length=1,choices=PAYMENT_STATUS_CHOICES,default=PAYMENT_STATUS_PENDING)
+  customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+  order = models.ForeignKey(Order,on_delete=models.PROTECT)
+  product = models.ForeignKey(Product,on_delete=models.PROTECT)
+  quantity = models.PositiveSmallIntegerField()
+  unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+
+class Address(models.Model):
+  street = models.CharField(max_length=255)
+  city = models.CharField(max_length=255)
+  customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+  
